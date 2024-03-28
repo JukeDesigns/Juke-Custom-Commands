0bdbc35d26a82dcc29873766307ccccc17afd3d5
JukeDesigns <codude147@gmail.com>
2024-03-28 14:37:06 -0400

    Update CustomCommands.cs

--- CustomCommands.cs
+++ CustomCommands.cs
@@ -1,6 +1,37 @@
-﻿namespace CustomCommands;
+﻿using CounterStrikeSharp.API.Core;
 
-public class Class1
+namespace CustomCommands;
+
+public class Commands : BasePlugin
 {
+    private readonly ILogger<Commands> _logger;
 
+    public Commands(ILogger<Commands> logger)
+    {
+        _logger = logger;
+    }
+
+    public void CustomCommandsLog()
+    {
+        _logger.LogInformation("Juke's Custom Commands Loading...")
+    }
+}
+
+public class CustomCommandsServiceCollection : IPluginServiceCollection<Juke-Custom-Command>
+{
+    public void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<Commands>();
+    }
+}
+
+[ConsoleCommand("discord", "Discord Link")]
+public void OnDiscordCommand(CCSPlayerController? player, CommandInfo command)
+{
+    if (player = null) {
+        Console.WriteLine("[blue]Join our Discord:[white] https://discord.gg/2cnMAqH548");
+        return;
+    }
+
+    Console.WriteLine("Join our Discord: https://discord.gg/2cnMAqH548")
 }
